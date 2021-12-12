63b7f46e0b2c9e1d20cd5c426e848302821afbf0
RobotCherries <zdip14@gmail.com>
2021-12-12 02:27:15 +0200

    Modified the register method to a working implementation, inside the auth controller.

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,13 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using NetAngularAuth.Data;
+using NetAngularAuth.DTOs;
+using NetAngularAuth.Models;
 
 namespace NetAngularAuth.Controllers
 {
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class AuthController : Controller
     {
-        [HttpGet("")]
-        public IActionResult Register() {
-            return Ok("Success");
+        private readonly IUserRepository _userRepository;
+
+        public AuthController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterDto dto)
+        {
+            if (dto == null)
+            {
+                return BadRequest("Please fill in all of the register fields");
+            }
+
+            System.Console.Write(dto);
+
+            var user = new User
+            {
+                Name = dto.Name,
+                Email = dto.Email,
+                Password = BCrypt.Net.BCrypt.HashPassword(dto.Password)
+            };
+
+            var result = _userRepository.Create(user);
+
+            return Created("Success", result);
         }
     }
 }
